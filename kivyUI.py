d88666e98385eca94b6cecba1a6a220464cb2c8c
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2019-05-19 19:41:16 +0200

    Now it is working. (Kind off)

--- kivyUI.py
+++ kivyUI.py
@@ -6,37 +6,53 @@ os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from time import time
+import time
 
+plt.ion()
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
-times = [time()]
+times = [time.ctime()]
 peoples = [0]
 plt.plot(times,peoples)
 
 ax1.plot(times, peoples)
 
+
 class TestApp(App):
     title = "Graph app"
 
     def build(self):
-        box = BoxLayout()
-        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
-        inbutton = Button(text = "Inpt")
-        inbutton.bind(on_press = self.inp)
+        box = GridLayout()
+        box.cols = 1
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf(),size_hint_y=50))
+        self.innum = TextInput(text='Hello world', multiline=False, size_hint_y = 10)
+        self.innum.bind(on_text_validate = self.on_enter)
+        inbutton = Button(text = "Input", size_hint_y = 10)
+        inbutton.bind(on_press = self.on_enter)
+        box.add_widget(self.innum)
         box.add_widget(inbutton)
         return box
     def inp(self, instance):
-        times.append(time())
-        peoples.append(input("Kolik?"))
+        times.append(time.ctime())
+        p = input("Kolik?")
+        peoples.append(int(p))
         ax1.clear()
-        ax1.plot(times,peoples)
+        ax1.plot(times, peoples)
+    def on_enter(self,instance, value = 5):
+        times.append(time.ctime())
+        p = self.innum.text
+        peoples.append(int(p))
+        ax1.clear()
+        ax1.plot(times, peoples)
 
 
 if __name__ == "__main__":
-    TestApp().run()
+    MyApp = TestApp()
+    MyApp.run()
